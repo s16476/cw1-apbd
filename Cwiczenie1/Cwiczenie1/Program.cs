5499968ffd455663d3f3a60ef76dad5b3e52f72e
Daniel Pawłowski <eldan3k@gmail.com>
2020-02-29 18:01:30 +0100

    Dokonczone

--- Cwiczenie1/Cwiczenie1/Program.cs
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cwiczenie1
@@ -10,13 +11,21 @@ namespace Cwiczenie1
         //string z małej
         static async Task Main(string[] args)
         {
+            var url = args.Length > 0 ? args[0] : "https://www.pjatk.edu.pl";
             var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pjatk.edu.pl");
+            var result = await client.GetAsync(url);
 
             if (result.IsSuccessStatusCode) //kod 2xx
             {
                 var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", 
+                    RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
 
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
 
             }
 
