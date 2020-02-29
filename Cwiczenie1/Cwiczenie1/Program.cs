f342ab11b2dd1b671e9b60c929ca89c205223285
Daniel Pawłowski <eldan3k@gmail.com>
2020-02-29 17:47:38 +0100

    Dodany projekt

---
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cwiczenie1
+{
+    class Program
+    {
+        //PascalCase - metoda z wielkiej
+        //string z małej
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pjatk.edu.pl");
+
+            if (result.IsSuccessStatusCode) //kod 2xx
+            {
+                var html = await result.Content.ReadAsStringAsync();
+
+
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
